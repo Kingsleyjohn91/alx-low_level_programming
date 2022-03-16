13e4d08655236acd7612379d3371b42cf5dbeddd
kinglname <kingsmail>
2022-03-15 17:09:09 -0700

    nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-long int i, j, k, next;
+	long int i, j, k, next;
 
-j = 1;
+	j = 1;
 
-k = 2;
+	k = 2;
 
-for (i = 1; i <= 50; ++i)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
