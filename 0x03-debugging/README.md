bae2dd45704d478d56d7bef941629f700e9093e7
kinglname <kingsmail>
2022-03-17 04:20:34 -0700

    Main.h

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This readme is for 0x03-debugging to test the functions that point if the integer is positive or negative.
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/** 
+ * main = test functions that point if the integer is positive or negative
+ * Return (0)
+ * */
+
+int main(void)
+{ 
+	int i;
+
+	i = 0;
+	
+	positive_or_negative(1);
+	
+	Return (0);
+}
