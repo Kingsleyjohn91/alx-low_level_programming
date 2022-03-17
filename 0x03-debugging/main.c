78fe4c757dc489c3887112cffe80f08041d672cd
kinglname <kingsmail>
2022-03-17 04:37:49 -0700

    Debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main test functions that point if integer is positive or negetive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative
+
+	Return (0);
+}
