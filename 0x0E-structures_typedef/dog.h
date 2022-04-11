64a6d06168274261c7d3369359dd246e4e2d6659
kinglname <kingsmail>
2022-04-11 14:43:13 -0700

    typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - combines three members
+ * @name: first member
+ * @age: second member which displays age
+ * @owner: third member which displays owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /*DOG_H*/
